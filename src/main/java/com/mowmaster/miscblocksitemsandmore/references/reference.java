f50dfdf19ca95108068f7a56863a9b04378cd916
Mowmaster <mowmaster@outlook.com>
2017-12-21 15:20:28 -0600

    first commit of 1.12

--- src/main/java/com/mowmaster/miscblocksitemsandmore/references/reference.java
+++ src/main/java/com/mowmaster/miscblocksitemsandmore/references/reference.java
@@ -3,10 +3,10 @@ package com.mowmaster.miscblocksitemsandmore.references;
 
 public class reference
 {
-    public static final String modid = "miscblocksitemsandmore";
+    public static final String modid = "mbim";
     public static final String modname = "NEoA";
     public static final String version = "0.2.5";
 
-    public static final String clientside = "com.mowmaster.miscblocksitemsandmore.proxies.ClientProxy";
-    public static final String serverside = "com.mowmaster.miscblocksitemsandmore.proxies.CommonProxy";
+    public static final String clientside = "com.mowmaster.mbim.proxies.ClientProxy";
+    public static final String serverside = "com.mowmaster.mbim.proxies.CommonProxy";
 }
