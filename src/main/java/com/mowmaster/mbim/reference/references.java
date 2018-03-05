9b8c2d5e6cccec195454a594c6aea3dff23bf039
Mowmaster <mowmaster@outlook.com>
2018-03-05 13:44:40 -0600

    pre build commit

--- src/main/java/com/mowmaster/mbim/reference/references.java
+++ src/main/java/com/mowmaster/mbim/reference/references.java
@@ -7,7 +7,7 @@ public class references
 {
     public static final String MODID = "mbim";
     public static final String MODNAME = "Miscellaneous Blocks, Items, and More";
-    public static final String VERSION = "0.000.1";
+    public static final String VERSION = "0.000.2";
 
     public static final String CLIENT_SIDE = "com.mowmaster.mbim.proxies.ClientProxy";
     public static final String SERVER_SIDE = "com.mowmaster.mbim.proxies.CommonProxy";
